631fe79f3aee8ee8333bd91cc9ffa2181807eee2
Shubbu-07 <95517382+Shubbu-07@users.noreply.github.com>
2024-12-24 22:57:22 +0530

    Added theme colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2ea4ff',
+        secondary: '#333333',
+        accent: '#8fd2ff',
+        background: '#121212',
+        text: '#f0f0f0'
+      },
+    },
   },
   plugins: [],
 }
